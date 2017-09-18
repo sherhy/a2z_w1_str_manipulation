1457b520583be40f024ef8b58b238596ee6a1038
hyunjin <hyunjin.jeong@toki.waseda.jp>
2017-09-18 19:03:35 +0900

    to parse words from loadstring and create each word as a div; (think of next processes later)

--- sketch.js
+++ sketch.js
@@ -1,8 +1,48 @@
-function setup() {
-	noCanvas();
-	let v = createP('hi')
+let words = [];
+let x = 100;
+let y = 50;
+
+class Word {
+	constructor(x,y){
+		this.x = x;
+		this.y = y;
+		this.div = createDiv();
+	}
+
+	pushword(word) {
+		this.div.html(word);
+		this.div.position(this.x,this.y);
+	}
+
+	update(){
+		this.y += 0;
+		this.div.position(this.x,this.y);
+
+		if (this.y> windowHeight){
+			this.y = 0;
+		}
+	}
+
 }
 
+
+function setup() {
+	noCanvas();
+
+	for (i = 0; i < 4; i++){
+		let v = new Word(x,y);
+		v.pushword('letsgo');
+		words.push(v);
+
+		x += 40;
+	}	
+}
+
+
 function draw() {
+	for (i = 0; i < words.length; i++){
+		words[i].update();
+	}	
+
 
 }
